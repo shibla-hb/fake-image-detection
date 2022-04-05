60dc456b831fc80266c3abd4ed9e1ca6bc7e85b6
shibla-hb <shahushibu05@gmail.com>
2022-04-05 10:02:46 +0530

    updated

--- fake_image_detections.py
+++ fake_image_detections.py
@@ -89,15 +89,100 @@ def home():
        return redirect('/')
 
 
-@app.route('/upload_image')
+@app.route('/upload_image',methods=['get','post'])
 def imageupload():
-    def convert_to_ela_image(path, quality):
-        temp_filename = 'temp_file_name.jpg'
-        ela_filename = 'temp_ela.png'
+    if request.method=="POST":
+        photo=request.files['filefield']
+        date = datetime.datetime.now().strftime("%y%m%d-%H%M%S")
+        photo.save(r"C:\Users\HP\PycharmProjects\fake_image_detections\static\upld_images\\" + date + '.jpg')
+        path = "/static/upld_images/" + date + '.jpg'
 
-        image = Image.open(path).convert('RGB')
-        image.save(temp_filename, 'JPEG', quality=quality)
-        temp_image = Image.open(temp_filename)
+        def convert_to_ela_image(path, quality):
+            temp_filename = 'temp_file_name.jpg'
+            ela_filename = 'temp_ela.png'
+
+            image = Image.open(path).convert('RGB')
+            image.save(temp_filename, 'JPEG', quality=quality)
+            temp_image = Image.open(temp_filename)
+
+            ela_image = ImageChops.difference(image, temp_image)
+
+            extrema = ela_image.getextrema()
+            max_diff = max([ex[1] for ex in extrema])
+            if max_diff == 0:
+                max_diff = 1
+            scale = 255.0 / max_diff
+
+            ela_image = ImageEnhance.Brightness(ela_image).enhance(scale)
+
+            return ela_image
+
+        # real_image_path = r'C:\Users\HP\PycharmProjects\fake_image_detections\static\upld_images'
+        # Image.open(real_image_path)
+        #
+        # convert_to_ela_image(real_image_path, 90)
+        #
+        # fake_image_path = r'C:\Users\IDZ\Downloads\FakeImageDetector_master\casia\CASIA2\Tp\Tp_D_NRN_S_N_ani10171_ani00001_12458.jpg'
+        # Image.open(fake_image_path)
+        #
+        # convert_to_ela_image(fake_image_path, 90)
+
+        image_size = (128, 128)
+
+        def prepare_image(image_path):
+            return np.array(convert_to_ela_image(image_path, 90).resize(image_size)).flatten() / 255.0
+
+        X = []  # ELA converted images
+        Y = [] #0 for fake, 1 for real
+
+        import random
+        path = r'C:\Users\IDZ\Downloads\FakeImageDetector_master\casia\CASIA2\Au\\'
+        for dirname, _, filenames in os.walk(path):
+            for filename in filenames:
+                if filename.endswith('jpg') or filename.endswith('png'):
+                    full_path = os.path.join(dirname, filename)
+                    X.append(prepare_image(full_path))
+                    Y.append(1)
+                    if len(Y) % 500 == 0:
+                        print(f'Processing {len(Y)} images')
+
+                        print(len(X), len(Y))
+                        X = np.array(X)
+                        Y = to_categorical(Y, 2)
+                        X = X.reshape(-1, 128, 128, 3)
+
+                        X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=5)
+                        X = X.reshape(-1, 1, 1, 1)
+                        print(len(X_train), len(Y_train))
+                        print(len(X_val), len(Y_val))
+
+                        path = r'C:\Users\IDZ\Downloads\FakeImageDetector_master\casia\CASIA2\Tp\\'
+                        for dirname, _, filenames in os.walk(path):
+                            for filename in filenames:
+                                if filename.endswith('jpg') or filename.endswith('png'):
+                                    full_path = os.path.join(dirname, filename)
+                                    X.append(prepare_image(full_path))
+                                    Y.append(0)
+                                    if len(Y) % 500 == 0:
+                                        print(f'Processing {len(Y)} images')
+
+                                        print(len(X), len(Y))
+                                        X = np.array(X)
+                                        Y = to_categorical(Y, 2)
+                                        X = X.reshape(-1, 128, 128, 3)
+
+                                        X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2,
+                                                                                          random_state=5)
+                                        X = X.reshape(-1, 1, 1, 1)
+                                        print(len(X_train), len(Y_train))
+                                        print(len(X_val), len(Y_val))
+
+        return "ok"
+
+
+
+
+    else:
         return render_template('upld_img.html')
 
 @app.route('/logout')
